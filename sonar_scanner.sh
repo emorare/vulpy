136c2c0842528f86439576450248a12391c6a9fd
emorare <89947717+emorare@users.noreply.github.com>
2021-09-15 20:02:15 -0600

    Update sonar_scanner.sh

--- sonar_scanner.sh
+++ sonar_scanner.sh
@@ -2,15 +2,12 @@ echo Installing Sonar Scanner CLI
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip
 unzip sonar-scanner-cli-4.6.2.2472-linux.zip
 export PATH=$(pwd)/sonar-scanner-cli-4.6.2.2472-linux/bin:$PATH
-
-echo Sonar Scanner installed!
-echo Running Sonar Scanner
-
-# We add here the command that runs Sonar Scanner
-sonar-scanner \
-  -Dsonar.projectKey=edgar-mora-sast-lab-1 \
+echo Sonar Scanner Installed!
+echo Running Sonar Scanner...
+# We add here the command that runs sonar scanner
+$(pwd)/sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner -X \
+  -Dsonar.projectKey=class-example \
   -Dsonar.sources=. \
   -Dsonar.host.url=https://sq.semanticsecurity.org \
-  -Dsonar.login=$SONARCUBE_LOGIN
-
+  -Dsonar.login=$SONARQUBE_LOGIN
 echo Done!
